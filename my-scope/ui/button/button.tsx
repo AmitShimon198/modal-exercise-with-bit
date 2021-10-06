01df7abc31bb24e3dbd38dd9cf13ef0570039603
amitvolga <amit.shimon@gmail.com>
2021-10-06 22:40:48 +0300

    adding class name to button

--- my-scope/ui/button/button.tsx
+++ my-scope/ui/button/button.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import classes from './button.module.css';
 export type ButtonProps = {
   children: JSX.Element | JSX.Element[] | string,
+  className?: string,
   onClick: React.MouseEventHandler,
 };
 
-export function Button({ children, onClick }: ButtonProps) {
+export function Button({ className, children, onClick }: ButtonProps) {
   return (
-    <button onClick={onClick} className={classes.button}>
+    <button onClick={onClick} className={`${classes.button} ${className}`}>
       {children}
     </button>
   );
